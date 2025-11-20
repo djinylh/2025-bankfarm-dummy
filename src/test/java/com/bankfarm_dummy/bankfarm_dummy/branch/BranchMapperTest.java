ee96537009adb667f2be79880939dd496442bbc5
djinylh <d.jinylh@gmail.com>
2025-11-20 20:18:49 +0900

    [fix] 지점 수정

--- src/test/java/com/bankfarm_dummy/bankfarm_dummy/branch/BranchMapperTest.java
+++ src/test/java/com/bankfarm_dummy/bankfarm_dummy/branch/BranchMapperTest.java
@@ -12,9 +12,8 @@ import java.util.Random;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BranchMapperTest extends Dummy {
-    final int ADD_ROW_COUNT = 5;
-    final int ADD_ROW_COUNT_TWO = 4;
-    final int ADD_ROW_COUNT_THREE = 5;
+    final int ADD_ROW_COUNT = 22;
+    final int ADD_ROW_COUNT_TWO = 12;
 
     @Test
     void branchMapperTest() {
@@ -24,67 +23,63 @@ class BranchMapperTest extends Dummy {
 
     for(int i=0;i<ADD_ROW_COUNT;i++) {
 
-        String[] city = {"서울","경기도","대구","부산", "경북"};
+        String[] city = {"서울","경기도","대구","부산", "경주", "인천","강원도"
+                ,"제주", "대전", "울산","밀양", "광주", "군산", "청주","나주"
+                ,"원주","강릉","여수","안동","진주","강릉","논산"
+        };
 
         for(int j=0;j<ADD_ROW_COUNT_TWO;j++) {
-            String[] district = {"동구", "서구", "중구", "북구"};
+            String[] district = {"동구", "서구", "중구", "북구","율하구","서면구","남동구", "분당구","해운대구","수성구","동래구","송파구"};
             String branchName = city[i] + district[j] + "점";
 
+            String[] address = {"현풍읍", "상인동", "삼성동", "상리읍", "서면"};
+            int addIdx = (int) (Math.random() * address.length);
 
-            for (int k = 0; k < ADD_ROW_COUNT_THREE; k++) {
-                String[] address = {"현풍읍", "상인동", "삼성동", "상리읍", "서면"};
+            Random red = new Random();
+            StringBuilder sb = new StringBuilder();
+            sb.append(red.nextInt(800));
 
-                Random red = new Random();
-                StringBuilder sb = new StringBuilder();
-                for (int d = 0; d < 5; d++) {
-                    sb.append(red.nextInt(10));
-                    if (d == 2) {
-                        sb.append("-");
-                    }
-
-                    String btanchaddress = city[i] + district[j] + address[k] + sb;
+            String btanchaddress = city[i] + district[j] + address[addIdx] + sb;
 
 
-                    double latitude = Double.parseDouble(kofaker.address().latitude());
-                    double longitude = Double.parseDouble(kofaker.address().longitude());
+            double latitude = Double.parseDouble(kofaker.address().latitude());
+            double longitude = Double.parseDouble(kofaker.address().longitude());
 
-                    // 개업일
-                    LocalDate start = LocalDate.of(1980, 1, 1);
-                    LocalDate end   = LocalDate.of(2023, 12, 31);
-                    long days = java.time.temporal.ChronoUnit.DAYS.between(start, end);
-                    LocalDate randomDate = start.plusDays((long)(Math.random() * days));
-
-
-                    //지역코드 랜덤
-                    StringBuilder bCode = new StringBuilder();
-                    for (int c = 0; c < 5; c++) {
-                        bCode.append(red.nextInt(10));
-                    }
-
-                    BranchReq req = new BranchReq();
-                    req.setBranNm(branchName);
-                    req.setBranTel(kofaker.phoneNumber().phoneNumber());
-                    req.setBranAddress(btanchaddress);
-                    req.setBranLatitude(latitude);
-                    req.setBranLongitude(longitude);
-                    req.setBranOpenedAt(randomDate);
-                    if(k==2){
-                        req.setBranActive("BR001");
-                    } else {
-                    req.setBranActive("BR003");
-                    }
-                    req.setBranRegionCd(bCode.toString());
-
-                    branchMapper.branchJoin(req);
-                    sqlSession.flushStatements();
+            // 개업일
+            LocalDate start = LocalDate.of(1980, 1, 1);
+            LocalDate end   = LocalDate.of(2023, 12, 31);
+            long days = java.time.temporal.ChronoUnit.DAYS.between(start, end);
+            LocalDate randomDate = start.plusDays((long)(Math.random() * days));
 
 
-                    }
+            //지역코드 랜덤
+            StringBuilder bCode = new StringBuilder();
+            for (int c = 0; c < 5; c++) {
+                bCode.append(red.nextInt(10));
+            }
 
-                }
+            BranchReq req = new BranchReq();
+            req.setBranNm(branchName);
+            req.setBranTel(kofaker.phoneNumber().phoneNumber());
+            req.setBranAddress(btanchaddress);
+            req.setBranLatitude(latitude);
+            req.setBranLongitude(longitude);
+            req.setBranOpenedAt(randomDate);
+            if(i==2){
+                req.setBranActive("BR001");
+            } else {
+                req.setBranActive("BR003");
+            }
+            req.setBranRegionCd(bCode.toString());
+
+            branchMapper.branchJoin(req);
+            sqlSession.flushStatements();
+
 
             }
 
+
         }
     }
+
 }
