e0369af8f6327184235498e8a2768bffec3153ce
djinylh <d.jinylh@gmail.com>
2025-11-20 19:22:03 +0900

    [docs] 근우님 클래스 복사

---
+++ src/main/java/com/bankfarm_dummy/bankfarm_dummy/card/model/CardOptionGetRes.java
@@ -0,0 +1,16 @@
+package com.bankfarm_dummy.bankfarm_dummy.card.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+@Getter
+@Setter
+@ToString
+public class CardOptionGetRes {
+
+    private String cardOptionCode;
+
+
+}
