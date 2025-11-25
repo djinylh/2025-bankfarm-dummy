bc12311b514177aafebeff490612e6796589c1aa
taewon1918 <taewon1918@gmail.com>
2025-11-25 18:14:37 +0900

    [test] 상담 더미데이터 수정

--- src/main/java/com/bankfarm_dummy/bankfarm_dummy/counseling/CounselingMapper.java
+++ src/main/java/com/bankfarm_dummy/bankfarm_dummy/counseling/CounselingMapper.java
@@ -3,10 +3,17 @@ package com.bankfarm_dummy.bankfarm_dummy.counseling;
 import com.bankfarm_dummy.bankfarm_dummy.counseling.model.CounselingReq;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface CounselingMapper {
 
     // 상담 등록
     int counselingJoin(CounselingReq counselingReq);
 
+    // 고객아이디 조회
+    List<Long> getCustId();
+
+    // 직원아이디 조회
+    List<Long> getEmpId();
 }
