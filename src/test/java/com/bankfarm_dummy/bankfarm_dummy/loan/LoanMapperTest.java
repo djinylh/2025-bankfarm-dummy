922a3bcde9b58d8f2a42a1b21c6c30293984d3a6
djinylh <d.jinylh@gmail.com>
2025-11-26 14:20:36 +0900

    [feat] 대출 더미데이터 제작

--- src/test/java/com/bankfarm_dummy/bankfarm_dummy/loan/LoanMapperTest.java
+++ src/test/java/com/bankfarm_dummy/bankfarm_dummy/loan/LoanMapperTest.java
@@ -7,9 +7,11 @@ import org.apache.ibatis.session.ExecutorType;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Random;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class LoanMapperTest extends Dummy {
 
@@ -22,14 +24,66 @@ class LoanMapperTest extends Dummy {
 
         List<LoanGetAppPkRes> loanGetAppPkRes = loanAppMapper.loanAppByCd002();
 
+        //랜덤 은행 코드
+        String[] bk ={"BK001","BK002","BK003","BK004","BK005",
+                "BK006","BK007","BK008","BK009","BK010","BK011",
+                "BK012","BK013","BK014","BK015","BK016","BK017",
+                "BK018","BK019","BK020"
+        };
+        int bkIdx = (int)(Math.random()*bk.length);
+
+
+
         for(LoanGetAppPkRes res : loanGetAppPkRes){
-            System.out.println(res.getLoanAppId());
+            // 만기일
+            LocalDate date =    res.getLoanDcsnDt().plusMonths(res.getLoanReqTrm());
+
+
+            LoanInsertReq req = new LoanInsertReq();
+            req.setLoanAppId(res.getLoanAppId());
+            req.setLoanNum(accountNum());
+            req.setLoanBnkCd(bk[bkIdx]);
+            req.setLoanFnIntrst(BigDecimal.valueOf(1.2));
+            req.setLoanEdDt(date);
+            req.setLoanFnRpmtTp("LN001");
+            req.setLoanUseAcct(accountNum2());
+            req.setLoanUseBnkCd(bk[bkIdx]);
+            req.setLoanDueSts("TX003");
+
+            loanMapper.loanInsert(req);
+            sqlSession.flushStatements();
+        }
+        sqlSession.close();
+
+
+
+    }
+    private String accountNum() {
+        Random randomAcct = new Random();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 11; i++) {
+            sb.append(randomAcct.nextInt(10));
+            if (i == 2 || i == 5) {
+                sb.append("-");
+            }
         }
 
-        LoanInsertReq req = new LoanInsertReq();
+        return sb.toString();
 
 
+    }
 
+    private String accountNum2() {
+        Random randomAcct = new Random();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 12; i++) {
+            sb.append(randomAcct.nextInt(10));
+            if (i == 3 || i == 5) {
+                sb.append("-");
+            }
+        }
+
+        return sb.toString();
 
 
     }
