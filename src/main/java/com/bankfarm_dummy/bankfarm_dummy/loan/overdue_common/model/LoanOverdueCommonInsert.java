26d5ff66d280e8474b8b55f2a49b201ec9e552b1
djinylh <d.jinylh@gmail.com>
2025-11-27 17:31:40 +0900

    [feat] 상품 ENUM 추가

--- src/main/java/com/bankfarm_dummy/bankfarm_dummy/loan/overdue_common/model/LoanOverdueCommonInsert.java
+++ src/main/java/com/bankfarm_dummy/bankfarm_dummy/loan/overdue_common/model/LoanOverdueCommonInsert.java
@@ -1,12 +1,17 @@
 package com.bankfarm_dummy.bankfarm_dummy.loan.overdue_common.model;
 
+import com.bankfarm_dummy.bankfarm_dummy.product_enum.ProductEnum;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDate;
 
 @Getter
 @Setter
 public class LoanOverdueCommonInsert {
     private long custId;
     private long odSourceId;
-    private
+    private ProductEnum odTp;
+    private LocalDate odStDt;
+    private long odAmt;
 }
