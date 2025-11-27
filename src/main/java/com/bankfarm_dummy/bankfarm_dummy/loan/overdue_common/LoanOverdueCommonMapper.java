b155301cbb698c72102f647769204232fc0e2ad0
djinylh <d.jinylh@gmail.com>
2025-11-27 17:30:35 +0900

    [feat] 상품 ENUM 추가

---
+++ src/main/java/com/bankfarm_dummy/bankfarm_dummy/loan/overdue_common/LoanOverdueCommonMapper.java
@@ -0,0 +1,4 @@
+package com.bankfarm_dummy.bankfarm_dummy.loan.overdue_common;
+
+public interface LoanOverdueCommonMapper {
+}
---
+++ src/main/java/com/bankfarm_dummy/bankfarm_dummy/loan/overdue_common/model/LoanOverdueCommonInsert.java
@@ -0,0 +1,12 @@
+package com.bankfarm_dummy.bankfarm_dummy.loan.overdue_common.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoanOverdueCommonInsert {
+    private long custId;
+    private long odSourceId;
+    private
+}
